9493c6e7aecaa750eedf5c54831f39c03cab8bfe
Janessa Aulén <43206084+s00190968@users.noreply.github.com>
2018-11-26 12:58:04 +0000

    form designed

---
+++ Labsheet8/RandomNumberGame/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomNumberGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        int num1, num2;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void guessBtn_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = rand.Next(0, 21);
+            if (biggerRadioBtn.IsChecked == true)
+            {
+
+            }
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = rand.Next(0, 21);
+            initialNumberTxtBx.Text = num1.ToString();
+        }
+
+    }
+}
